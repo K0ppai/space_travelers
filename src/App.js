38d53051d1af76899cbfa7bef236a6ac10a02b63
koppai <psthu.koppai@gmail.com>
2023-07-24 17:42:54 +0630

    Use ruotes for mission component in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Missions from './components/missions/missions';
+import { Route,Routes } from 'react-router';
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route
+          path="/missions"
+          element={<Missions />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
