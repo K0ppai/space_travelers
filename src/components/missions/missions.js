7007a89233557e8d1b03e2ef7b646a2d450d3406
koppai <psthu.koppai@gmail.com>
2023-07-24 18:47:37 +0630

    Use `useEffect` hook in `Missions` component

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../../redux/missions/missionsSlice';
+
 function Missions() {
-  return <div>Mission page</div>;
+  const missions = useSelector((state) => state.missions.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Missions</h2>
+      <ul>
+        {missions.map((mission) => (
+          <li key={mission.mission_id}>{mission.mission_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Missions;
