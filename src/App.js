6430725b87a8e6c66ea9b307613ab53f64ecaaa3
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-24 13:19:32 +0300

    Nav Bar Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Details from './pages/Details';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/details" element={<Details />} />
-      </Routes>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
