06a79df640e65e356e0944e2342c7b81d25d63da
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-24 13:12:42 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
