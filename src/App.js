c024da5779d97ddd8c4caadeb06044c4ba382196
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-24 15:11:22 +0300

    Add Fetch rockets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import Navbar from './components/Navbar';
 import Missions from './components/missions/missions';
 import Rockets from './pages/Rockets';
+import { fetchRockets } from './redux/Rockets/rocket';
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Routes>
         <Route
