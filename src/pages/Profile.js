4e229f1fadd20eef15364cdeefbc9e41c41cb7da
koppai <psthu.koppai@gmail.com>
2023-07-25 21:09:00 +0630

    Import ActiveMissions component to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,10 @@
 import ActiveRockets from '../components/ActiveRockets';
+import ActiveMissions from '../components/missions/ActiveMissions';
 
 function Profile() {
   return (
     <div className="myProfileContainer">
+      <ActiveMissions />
       <ActiveRockets />
     </div>
   );
